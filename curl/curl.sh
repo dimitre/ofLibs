19ea28df346d33be8e3125612d754add450efa7d
Dimitre <dimitre@users.noreply.github.com>
2025-01-05 01:11:49 -0300

    ok

---
+++ curl/curl.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+cd "$(dirname "$0")"
+LIB="curl"
+# DRYRUN="echo --------->>>> echo"
+DRYRUN=""
+FOLDERIOS=${LIB}-ios
+FOLDERSIM=${LIB}-sim
+
+${DRYRUN} mkdir ${FOLDERIOS}
+${DRYRUN} mkdir ${FOLDERSIM}
+
+COMMON="-DCMAKE_BUILD_TYPE=Release \
+-DBUILD_SHARED_LIBS=OFF \
+-DCMAKE_BUILD_TYPE=Release \
+-DBUILD_EXAMPLES=OFF \
+-DBUILD_CURL_EXE=OFF \
+-DCMAKE_USE_OPENSSL=ON \
+-DCMAKE_TOOLCHAIN_FILE=../../_ios-cmake/ios.toolchain.cmake \
+"
+
+${DRYRUN} cmake -S chalet_external/${LIB} -B ${FOLDERIOS} -G Xcode ${COMMON} \
+    -DPLATFORM=OS64
+${DRYRUN} cmake --build ${FOLDERIOS} --config Release
+
+${DRYRUN} cmake -S chalet_external/${LIB} -B ${FOLDERSIM} -G Xcode ${COMMON} \
+    -DPLATFORM=SIMULATOR64COMBINED
+${DRYRUN} cmake --build ${FOLDERSIM} --config Release
+
+${DRYRUN} rm -rf include
+${DRYRUN} mkdir include
+${DRYRUN} cp chalet_external/${LIB}/*.h include
+
+xcframework_flags=""
+xcframework_flags+=" -library ${FOLDERSIM}/code/lib/Release/*.a"
+xcframework_flags+=" -library ${FOLDERIOS}/code/lib/Release/*.a"
+xcframework_flags+=" -headers include"
+
+${DRYRUN} xcodebuild -create-xcframework ${xcframework_flags} -output ${LIB}.xcframework
+
+${DRYRUN} mkdir dist
+${DRYRUN} zip -r dist/${LIB}.zip ${LIB}.xcframework
