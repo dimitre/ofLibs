ded530de23810af4ab05f9f51dc09558ca24908d
Dimitre <dimitre@users.noreply.github.com>
2025-01-05 03:07:33 -0300

    better ios script for tess2, reproduce in others

--- tess2/tess2.sh
+++ tess2/tess2.sh
@@ -2,6 +2,11 @@
 cd "$(dirname "$0")"
 LIB="tess2"
 # DRYRUN="echo --------->>>> echo"
+
+executa() {
+    echo -e "\033[0;35m$@ \033[0m"
+    "$@"
+}
 DRYRUN=""
 FOLDERIOS=${LIB}-ios
 FOLDERSIM=${LIB}-sim
@@ -15,24 +20,22 @@ COMMON="-DCMAKE_BUILD_TYPE=Release \
 -DCMAKE_TOOLCHAIN_FILE=../../_ios-cmake/ios.toolchain.cmake \
 "
 
-${DRYRUN} cmake -S chalet_external/${LIB} -B ${FOLDERIOS} -G Xcode ${COMMON} \
-    -DPLATFORM=OS64
-${DRYRUN} cmake --build ${FOLDERIOS} --config Release
+executa cmake -S chalet_external/${LIB} -B ${FOLDERIOS} -G Xcode ${COMMON} -DPLATFORM=OS64
+executa cmake --build ${FOLDERIOS} --config Release
 
-${DRYRUN} cmake -S chalet_external/${LIB} -B ${FOLDERSIM} -G Xcode ${COMMON} \
-    -DPLATFORM=SIMULATOR64COMBINED
-${DRYRUN} cmake --build ${FOLDERSIM} --config Release
+executa cmake -S chalet_external/${LIB} -B ${FOLDERSIM} -G Xcode ${COMMON} -DPLATFORM=SIMULATOR64COMBINED
+executa cmake --build ${FOLDERSIM} --config Release
 
-${DRYRUN} rm -rf include
-${DRYRUN} mkdir include
-${DRYRUN} cp chalet_external/${LIB}/*.h include
+executa rm -rf include
+executa mkdir include
+executa cp chalet_external/${LIB}/Include/*.h include
 
 xcframework_flags=""
-xcframework_flags+=" -library ${FOLDERSIM}/code/lib/Release/*.a"
-xcframework_flags+=" -library ${FOLDERIOS}/code/lib/Release/*.a"
+xcframework_flags+=" -library ${FOLDERSIM}/Release-iphonesimulator/*.a"
+xcframework_flags+=" -library ${FOLDERIOS}/Release-iphoneos/*.a"
 xcframework_flags+=" -headers include"
 
-${DRYRUN} xcodebuild -create-xcframework ${xcframework_flags} -output ${LIB}.xcframework
+executa xcodebuild -create-xcframework ${xcframework_flags} -output ${LIB}.xcframework
 
-${DRYRUN} mkdir dist
-${DRYRUN} zip -r dist/${LIB}.zip ${LIB}.xcframework
+executa mkdir dist
+executa zip -r dist/${LIB}.zip ${LIB}.xcframework
