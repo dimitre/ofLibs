ec20302fd4a62f60a38cdc8d0384292d645ff8cc
Dimitre <dimitre@users.noreply.github.com>
2025-01-14 14:50:41 -0300

    install.sh

---
+++ install.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+cd "$(dirname "$0")"
+
+OF_FOLDER=/Volumes/tool/ofw
+LIBS_FOLDER=${OF_FOLDER}/libs
+
+COLOR='\033[0;32m'
+COLOR2='\033[0;34m'
+NC='\033[0m' # No Color
+
+section() {
+    echo ""
+    printf "💾 ${COLOR}"
+    echo $@
+    printf "${NC}"
+}
+
+executa () {
+    printf ${COLOR2}
+    echo "✅ --> " $@
+    printf ${NC}
+    # printf ${COLOR2}"✅ --> " $@  "${NC}\n"
+    printf ${NC}
+    $@
+}
+
+instala() {
+    W="wget2 "
+    LIBNAME=$@
+    section Installing ${LIBNAME}
+    DOWNLOAD=oflib_${LIBNAME}_osx.zip
+    executa ${W} https://github.com/dimitre/ofLibs/releases/download/v0.12.1/${DOWNLOAD} -O ${DOWNLOAD}
+    executa unzip -o ${DOWNLOAD} -d ${LIBS_FOLDER}/${LIBNAME}
+}
+
+executa mkdir _download
+executa cd _download
+
+# if [ -d ${ADDON_FOLDER} ]; then
+#     echo "Directory exists: ${ADDON_FOLDER}"
+#     # open  ${ADDON_FOLDER}
+#     # mv ${ADDON_FOLDER} ${ADDON_FOLDER}_bak
+# else
+#     echo "Directory does not exist: ${ADDON_FOLDER}"
+# fi
+
+# --no-clobber
+# # unzip -o = overwrite without prompting
+
+instala assimp
+instala glm
+instala curl
