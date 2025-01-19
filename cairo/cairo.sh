59b33c7d74dd16e97f88b919b7a72a2d97812b67
Dimitre <dimitre@users.noreply.github.com>
2025-01-19 01:48:20 -0300

    up

---
+++ cairo/cairo.sh
@@ -0,0 +1,22 @@
+brew install meson
+
+git clone https://gitlab.freedesktop.org/cairo/cairo --depth 1
+cd cairo
+git fetch --depth 1 origin 727966dfca933d4a8fc6e65a428e1a9ce1a2fec2
+
+meson setup build --buildtype=release
+meson build -Ddefault_library=static -Ddefault_both_libraries=static -Dbuildtype=release -Dxcb=disabled -Dxlib=disabled -Dlzo=disabled --reconfigure
+ninja -C build
+
+# cd chalet_external/cairo
+# meson setup --reconfigure builddir
+# meson configure -Dxlib=disabled -Dfontconfig=disabled -Dfreetype=disabled -Ddwrite=disabled
+# meson compile -C builddir
+# cd builddir
+# meson compile
+# meson test
+
+# echo meson build
+# meson build
+# echo meson compile -C build --xlib=false
+# meson compile -C build --xlib=false
