eba03cc3e56c304328ea905a16acacbbadbbd161
Dimitre <dimitre@users.noreply.github.com>
2025-01-19 01:29:44 -0300

    meson test on cairo

--- cairo/cairo/src/cairo-wideint-type-private.h
+++ cairo/cairo/src/cairo-wideint-type-private.h
@@ -47,11 +47,7 @@
 # include <inttypes.h>
 #elif HAVE_SYS_INT_TYPES_H
 # include <sys/int_types.h>
-#else
-#error Cannot find definitions for fixed-width integral types (uint8_t, uint32_t, etc.)
-#endif
-
-#if defined(_MSC_VER)
+#elif defined(_MSC_VER)
   typedef __int8 int8_t;
   typedef unsigned __int8 uint8_t;
   typedef __int16 int16_t;
@@ -63,6 +59,8 @@
 # ifndef HAVE_UINT64_T
 #  define HAVE_UINT64_T 1
 # endif
+#else
+#error Cannot find definitions for fixed-width integral types (uint8_t, uint32_t, etc.)
 #endif
 
 #ifndef INT16_MIN
