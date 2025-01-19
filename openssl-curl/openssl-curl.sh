89b170e5f05ea9a96cc9f9564fcf1b07da33f6f6
Dimitre <dimitre@users.noreply.github.com>
2025-01-19 20:39:15 -0300

    openssl curl using folder dist, so it follows Chalet. todo: same in Cairo

--- openssl-curl/openssl-curl.sh
+++ openssl-curl/openssl-curl.sh
@@ -3,6 +3,8 @@ cd "$(dirname "$0")"
 cd Build-OpenSSL-cURL
 ./build.sh -o 3.0.15 -c 8.11.1 -d -i 11.0 -a 11.0
 
+mkdir dist
+
 cd ..
 mkdir curl
 cd curl
@@ -11,7 +13,7 @@ cp -r Build-OpenSSL-cURL/curl/lib .
 cp Build-OpenSSL-cURL/curl/curl*/COPYING .
 mkdir lib/${PLATFORM}
 mv lib/*.a lib/${PLATFORM}
-zip -r ../oflib_curl_${PLATFORM}.zip lib include
+zip -r ../dist/oflib_curl_${PLATFORM}.zip lib include
 cd ..
 
 mkdir openssl
@@ -23,5 +25,5 @@ cp Build-OpenSSL-cURL/openssl/openssl*/LICENSE .
 mkdir lib/${PLATFORM}
 mv lib/*.a lib/${PLATFORM}
 
-zip -r ../oflib_openssl_${PLATFORM}.zip lib include
+zip -r ../dist/oflib_openssl_${PLATFORM}.zip lib include
 cd ..
