db772854b9cb8d0794cf7dce6fc139713f6edbce
Dimitre <dimitre@users.noreply.github.com>
2025-01-19 22:28:45 -0300

    rm curl

--- curl/test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cd "$(dirname "$0")"
-chalet build &&
-cp -rf build/arm64-apple-darwin_Release/ext.curl/lib/lib/libcurl.a /Volumes/tool/ofw/libs/curl/lib/macos
-cd /Volumes/tool/ofw/examples/3d/assimp3DModelLoaderExample
-make && make RunRelease
