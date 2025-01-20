e2736112ae1a8dad2f9c830faef12d68de1a219b
Dimitre <dimitre@users.noreply.github.com>
2025-01-20 18:08:43 -0300

    opencv ok?

--- opencv/opencv_install.sh
+++ opencv/opencv_install.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 cd "$(dirname "$0")"
-#LIB="FreeImage"
-#echo $@
-PLATFORM=macos
-cmake --install ./build/arm64-apple-darwin_Release/ext.opencv
-mkdir Release/lib/${PLATFORM}
-mv Release/lib/{opencv4,*.a} Release/lib/${PLATFORM}
-#printenv
+
+# only for running script locally
+if [ -z ${var+x} ]; then
+    PLATFORM=macos
+fi
+
+executa() {
+    printf "✅ ${COLOR2} ${@} ${NC}\n\r"
+    $@
+}
+
+executa cmake --install ./build/arm64-apple-darwin_Release/ext.opencv
+executa mkdir -p Release/lib/${PLATFORM}
+executa mv ./Release/lib/opencv4 ./Release/lib/${PLATFORM}
+executa mv ./Release/lib/*.a ./Release/lib/${PLATFORM}
