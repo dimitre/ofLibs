306c12f96d9cc4dca013faa9df0b0f56a06ab574
Dimitre <dimitre@users.noreply.github.com>
2025-02-26 11:26:44 -0300

    pixman

---
+++ pixman/pixman.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+cd "$(dirname "$0")"
+# pwd
+
+cd chalet_external/pixman
+meson setup build -Ddefault_library=static -Dbuildtype=release --reconfigure -Ddemos=disabled -Dtests=disabled
+ninja -C build
+
+mkdir -p lib/${PLATFORM}
+cp build/pixman/*.a lib/${PLATFORM}
+mkdir -p include/pixman
+cp -R build/pixman/*.h include/pixman
+# zip -r oflib_pixman_${PLATFORM}.zip lib include
+# mv *.zip ../..
+
+# cd ..
+# mkdir -p lib/${PLATFORM}
+# cp cairo/build/src/*.a lib/${PLATFORM}
+# mkdir -p include/cairo
+# cp -R cairo/src/*.h include/cairo
+# cp -R cairo/build/src/*.h include/cairo
+
+# zip -r oflib_cairo_${PLATFORM}.zip lib include
