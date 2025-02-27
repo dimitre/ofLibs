730f8355acd9f816910b329f834b58e2c847432e
Dimitre <dimitre@users.noreply.github.com>
2025-02-26 21:21:25 -0300

    cairo fat lib

--- cairo/cairo.sh
+++ cairo/cairo.sh
@@ -4,19 +4,44 @@ pwd
 # brew install meson
 # export CCACHE_DISABLE=1
 
-git clone https://gitlab.freedesktop.org/cairo/cairo --depth 1
+# git clone https://gitlab.freedesktop.org/cairo/cairo --depth 1
 cd cairo
-git fetch --depth 1 origin 727966dfca933d4a8fc6e65a428e1a9ce1a2fec2
+# git fetch --depth 1 origin 727966dfca933d4a8fc6e65a428e1a9ce1a2fec2
 
-meson setup build -Ddefault_library=static -Ddefault_both_libraries=static -Dbuildtype=release -Dxcb=disabled -Dxlib=disabled -Dlzo=disabled --reconfigure
-ninja -C build
+#
+#
+# meson setup build -Dc_args="-arch x86_64" -Dcpp_args="-arch x86_64" -Ddefault_library=static -Ddefault_both_libraries=static -Dbuildtype=release -Dxcb=disabled -Dxlib=disabled -Dlzo=disabled --reconfigure
+# meson setup build -Dc_args="-march=x86-64 -march=arm64" -Ddefault_library=static -Ddefault_both_libraries=static -Dbuildtype=release -Dxcb=disabled -Dxlib=disabled -Dlzo=disabled --reconfigure
+# ninja -C build
+
+meson setup --cross-file ../macos-x86_64.txt ../intel \
+-Ddefault_library=static -Ddefault_both_libraries=static -Dbuildtype=release \
+-Dxcb=disabled -Dxlib=disabled -Dlzo=disabled \
+-Dpng=disabled -Dzlib=disabled  \
+--reconfigure
+ninja -C ../intel
+# lipo -info ../intel/src/*.a
+
+meson setup ../silicon -Ddefault_library=static -Ddefault_both_libraries=static -Dbuildtype=release -Dxcb=disabled -Dxlib=disabled -Dlzo=disabled --reconfigure
+ninja -C ../silicon
+# lipo -info ../silicon/src/*.a
 
 cd ..
+
+lipo -info ./intel/src/*.a
+lipo -info ./silicon/src/*.a
+
+
+
+# cd ..
 mkdir -p lib/${PLATFORM}
-cp cairo/build/src/*.a lib/${PLATFORM}
+lipo ./intel/src/*.a ./silicon/src/*.a -create -output ./lib/${PLATFORM}/libcairo.a
+lipo -info ./lib/${PLATFORM}/libcairo.a
+
+# cp cairo/build/src/*.a lib/${PLATFORM}
 mkdir -p include/cairo
-cp -R cairo/src/*.h include/cairo
-cp -R cairo/build/src/*.h include/cairo
+cp -R ./intel/src/*.h include/cairo
+# cp -R cairo/build/src/*.h include/cairo
 
 zip -r oflib_cairo_${PLATFORM}.zip lib include
 
