7443fb53c2e6daccc1138a64760dfe1d3c299859
Dimitre <dimitre@users.noreply.github.com>
2025-08-23 10:10:37 -0300

    assimp cleanup

--- pixman/pixman.sh
+++ pixman/pixman.sh
@@ -3,12 +3,12 @@ cd "$(dirname "$0")"
 # pwd
 #
 # export
-# export MACOSX_DEPLOYMENT_TARGET=11.5
+# export MACOSX_DEPLOYMENT_TARGET=11.6
 unset MACOSX_DEPLOYMENT_TARGET
-export MIN_SUPPORTED_MACOSX_DEPLOYMENT_TARGET=11.5
-export LDFLAGS='-mmacosx-version-min=11.5'
-export CFLAGS='-stdlib=libc++ -mmacosx-version-min=11.5'
-export CXXFLAGS='-stdlib=libc++ -mmacosx-version-min=11.5'
+export MIN_SUPPORTED_MACOSX_DEPLOYMENT_TARGET=11.6
+export LDFLAGS='-mmacosx-version-min=11.6'
+export CFLAGS='-stdlib=libc++ -mmacosx-version-min=11.6'
+export CXXFLAGS='-stdlib=libc++ -mmacosx-version-min=11.6'
 
 git clone https://gitlab.freedesktop.org/pixman/pixman.git --depth 1
 cd pixman
